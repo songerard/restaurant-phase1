51c05c37a7ece92e23af93cf51b858d71cad811b
songerard <songerard@gmail.com>
2021-09-30 08:36:42 +0800

    Set index page

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ const app = express()
 
 // require express-handlebars
 const exphbs = require('express-handlebars')
-app.engine('hbs', exphbs({ defaultLayout: 'main' }))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
+
+// require bootstrap
+app.use(express.static('public'))
+
+// require restaurant.json
+const restaurantJSON = require('./restaurant.json')
+const restaurants = restaurantJSON.results
 
 // set port
 const port = 3000
@@ -13,4 +20,10 @@ const port = 3000
 // listen to localhost:3000
 app.listen(port, () => {
   console.log(`Express is listening to http://localhost:${port}`)
+})
+
+// set route
+// index page
+app.get('/', (req, res) => {
+  res.render('index', { restaurants })
 })
